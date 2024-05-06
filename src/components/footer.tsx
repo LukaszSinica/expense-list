fa5abc224e0a4c43b2e5693ec97ca956fe6c9786
LukaszSinica <lukasz.sinica@o2.pl>
2024-05-06 21:18:41 +0200

    adding placeholders

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-between ">
+        <div>Analitycs</div>
+        <div>add expense</div>
+        <div>changelog</div>
+    </footer>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav className="flex justify-between">
+        <div>dark screen</div>
+        <div>Expenses</div>
+        <div>brush</div>
+    </nav>
+  )
+}
