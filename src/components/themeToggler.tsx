188cc89ec59647fe9c3bd133ed466e3618e2e9e6
LukaszSinica <lukasz.sinica@o2.pl>
2024-06-16 01:14:34 +0200

    fix

--- src/components/themeToggler.tsx
+++ src/components/themeToggler.tsx
@@ -5,20 +5,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 export default function ThemeToggler() {
-    const { theme, setTheme } = useTheme();
+    const { resolvedTheme, setTheme } = useTheme()
+    const [hasMounted, setHasMounted] = React.useState(false);
 
-    const handleThemeChange = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
+
+    const label = resolvedTheme === 'dark' ? 'Activate light mode' : 'Activate dark mode'
+
+    React.useEffect(() => {
+        setHasMounted(true);
+      }, []);
+
+    if (!hasMounted) {
+        return <FontAwesomeIcon icon={faMoon} className='w-5 h-5' />
     }
 
+	const toggleTheme = () => {
+		const newTheme = resolvedTheme === 'light' ? 'dark' : 'light'
+		setTheme(newTheme)
+	}
+
     return (
-        <button onClick={handleThemeChange} className="theme-toggler">
-            {
-                theme === 'dark' ? (
-                <FontAwesomeIcon icon={faSun} className='w-5 h-5' />
+        <button onClick={toggleTheme} className="theme-toggler" aria-label={label} title={label}>
+            { 
+                resolvedTheme == 'light' ? (
+                    <FontAwesomeIcon icon={faMoon} className='w-5 h-5' />
                 ) : (
-                <FontAwesomeIcon icon={faMoon} className='w-5 h-5' />
+                    <FontAwesomeIcon icon={faSun} className='w-5 h-5' />
                 )
+
             }
         </button>
     );
